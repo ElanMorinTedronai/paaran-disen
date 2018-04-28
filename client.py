c3a7940b86079bbfdbebeda2238c7d8383a80093
ElanMorinTedronai <saburovser@gmail.com>
2018-04-28 16:21:01 +0300

    Ready to Engage

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 1234))
-while True:
- req = input()
- s.send(req)
- rsp = s.recv(1024)
- print(rsp)
-s.close()
