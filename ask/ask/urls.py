66e15337d554961252ec136365554fe3c04da751
ElanMorinTedronai <saburovser@gmail.com>
2018-04-30 17:17:04 +0300

    Modified urls for jango version lower than 2

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', include('qa.urls')),
-    path('signup/', include('qa.urls')),
-    path('question/', include('qa.urls')),
-    path('ask/', include('qa.urls')),
-    path('popular/', include('qa.urls')),
-    path('new/', include('qa.urls')),
-    path('', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^question/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
+    url(r'^', include('qa.urls')),
 ]
